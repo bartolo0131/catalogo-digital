8ba7557158f984a2b209713df8a107f6c4b4c430
bartolo0131 <edilson_saenz@hotmail.com>
2026-01-29 20:11:34 -0500

    Configura rutas productos correctamente

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "catalogo",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error("Error BD:", err);
+    console.error("❌ Error BD:", err);
   } else {
-    console.log("Conectado a MySQL");
+    console.log("✅ Conectado a MySQL");
   }
 });
 
